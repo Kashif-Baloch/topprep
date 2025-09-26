6180c602bbc72ff01b5862b860cfa75d17d34798
Kashif <kashifkhan451810@gmail.com>
2025-09-26 16:02:08 +0500

    add: gpus apis integrations

--- src/app/sub-page/[sub_page]/page.tsx
+++ src/app/sub-page/[sub_page]/page.tsx
@@ -1,6 +1,6 @@
+import ClientWrapper from "@/app/videos/[sub_pages]/ClientWrapper";
 import Footer from "@/components/layouts/Footer";
 import Navbar from "@/components/layouts/Navbar";
-import ClientWrapper from "./ClientWrapper";
 
 const Page = async ({ params }: { params: Promise<{ sub_page: string }> }) => {
   const { sub_page } = await params;
@@ -8,11 +8,7 @@ const Page = async ({ params }: { params: Promise<{ sub_page: string }> }) => {
   return (
     <>
       <Navbar />
-<<<<<<<< HEAD:src/app/videos/[sub_pages]/page.tsx
-      <ClientWrapper sub_pages={sub_pages} />
-========
-      <SubPage sub_page={sub_page} />
->>>>>>>> ac0da70a75e13ddef8cdd07c6ec582e942b3e0d4:src/app/sub-page/[sub_page]/page.tsx
+      <ClientWrapper sub_pages={sub_page} />
       <Footer />
     </>
   );
